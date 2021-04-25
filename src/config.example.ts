4b969c740cf442fd041212744a4713f3463758c0
JS <r0blox1053@outlook.com>
2021-04-25 13:02:15 -1000

    Dog

---
+++ src/config.example.ts
@@ -0,0 +1,5 @@
+export const USER_TO_MATCH: number | string = 105986725;
+export const OUTFIT_CHECK_INTERVAL: number = 240;
+export const MAX_BUNDLE_COST: number = 750;
+export const COOKIE: string = '';
+export const LOGGING_WEBHOOK: string = '';
