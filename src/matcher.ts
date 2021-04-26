5ce570dbde25b96191aa3bfb678857dcc96922a4
JS <r0blox1053@outlook.com>
2021-04-26 07:33:00 -1000

    more

--- src/matcher.ts
+++ src/matcher.ts
@@ -1,6 +1,29 @@
 import Roblox from './roblox';
-import { COOKIE } from './config';
+import deepEqual from 'fast-deep-equal';
+import { COOKIE, USER_TO_MATCH, OUTFIT_CHECK_INTERVAL } from './config';
 
 const roblox = new Roblox(COOKIE);
 
-roblox.getAvatar().then(console.log);
+const checkAndMatchOutfit = async () => {
+    try {
+        const myAvatar = await roblox.getMyAvatar();
+        const matcherAvatar = await roblox.getAvatar(USER_TO_MATCH);
+        
+        const isScalesSame = deepEqual(myAvatar.scales, matcherAvatar.scales);
+        const isColorsSame = deepEqual(myAvatar.bodyColors, matcherAvatar.bodyColors);
+        const isAvatarTypeSame = myAvatar.playerAvatarType === matcherAvatar.playerAvatarType;
+        // const isAssetsSame = deepEqual(myAvatar.assets, matcherAvatar.assets);
+    
+        if (!isScalesSame) await roblox.setScales(matcherAvatar.scales);
+        if (!isAvatarTypeSame) await roblox.setAvatarType(matcherAvatar.playerAvatarType);
+        if (!isColorsSame) await roblox.setColors(matcherAvatar.bodyColors);
+
+        console.log(myAvatar, matcherAvatar)
+    } catch (err) {
+        console.error(err);
+    }
+
+    setTimeout(checkAndMatchOutfit, OUTFIT_CHECK_INTERVAL * 1000);
+};
+
+checkAndMatchOutfit();
--- src/roblox.ts
+++ src/roblox.ts
@@ -1,54 +1,127 @@
-import axios from 'axios';
+import axios from "axios";
 
-enum AvatarType {
-    R15 = 'R15',
-    R6 = 'R6'
+type AvatarType = "R15" | "R6";
+
+interface Scales {
+  height: number;
+  width: number;
+  head: number;
+  depth: number;
+  proportion: number;
+  bodyType: number;
+}
+
+interface Colors {
+  headColorId: number;
+  torsoColorId: number;
+  rightArmColorId: number;
+  leftArmColorId: number;
+  rightLegColorId: number;
+  leftLegColorId: number;
 }
 
 interface AvatarInfo {
-    scales: {
-        height: number;
-        width: number;
-        head: number;
-        depth: number;
-        proportion: number;
-        bodyType: number
+  scales: Scales;
+  playerAvatarType: AvatarType;
+  bodyColors: Colors;
+  assets: {
+    id: number;
+    name: string;
+    assetType: {
+      id: number;
+      name: string;
     };
-    playerAvatarType: AvatarType;
-    bodyColors: {
-        headColorId: number;
-        torsoColorId: number;
-        rightArmColorId: number;
-        leftArmColorId: number;
-        rightLegColorId: number;
-        leftLegColorId: number;
-    };
-    assets: {
-        id: number;
-        name: string;
-        assetType: {
-            id: number;
-            name: string;
-        }
-    }[];
-    defaultShirtApplied: boolean;
-    defaultPantsApplied: boolean;
+  }[];
+  defaultShirtApplied: boolean;
+  defaultPantsApplied: boolean;
+}
+
+interface AssetDetails {
+  id: number;
+  itemType: string;
+  assetType: string;
+  bundleType: string;
+  name: string;
+  description: string;
+  productId: number;
+  genres: string[];
+  bundledItems: {
+    owned: boolean;
+    id: number;
+    name: string;
+    type: string;
+  }[];
+  itemStatus: string[];
+  itemRestrictions: string[];
+  creatorType: "User";
+  creatorTargetId: 0;
+  creatorName: string;
+  price: 0;
+  premiumPricing: {
+    premiumDiscountPercentage: 0;
+    premiumPriceInRobux: 0;
+  };
+  lowestPrice: 0;
+  priceStatus: string;
+  unitsAvailableForConsumption: 0;
+  purchaseCount: 0;
+  favoriteCount: 0;
+  offSaleDeadline: "2021-04-26T01:01:20.028Z";
 }
 
 class Roblox {
-    public csrfToken: string | null;
+  public csrfToken: string | null;
 
-    constructor(cookie: string) {
-        this.csrfToken = null;
-        axios.defaults.headers.common['Cookie'] = `.ROBLOSECURITY=${cookie}`;
-        axios.defaults.headers.common['User-Agent'] = 'OutfitMatcher/Axios';
-    }
+  constructor(cookie: string) {
+    this.csrfToken = null;
+    axios.defaults.headers.common["Cookie"] = `.ROBLOSECURITY=${cookie}`;
+    axios.defaults.headers.common["User-Agent"] = "OutfitMatcher/Axios";
+    axios.interceptors.response.use()
+  }
 
-    public async getAvatar(): Promise<AvatarInfo> {
-        const res = await axios(`https://avatar.roblox.com/v1/avatfrwar`);
+  public async getAvatar(userId: number | string): Promise<AvatarInfo> {
+    const res = await axios(
+      `https://avatar.roblox.com/v1/users/${userId}/avatar`
+    );
 
-        return res.data;
-    }
+    return res.data;
+  }
+
+  public async getMyAvatar(): Promise<AvatarInfo> {
+    const res = await axios(`https://avatar.roblox.com/v1/avatar`);
+
+    return res.data;
+  }
+
+  public async setAvatarType(type: AvatarType): Promise<void> {
+    const res = await axios.post(`https://avatar.roblox.com/v1/avatar/set-player-avatar-type`, {
+      playerAvatarTypeModel: type
+    });
+
+    if(!res.data.success) return Promise.reject(res);
+
+    return;
+  }
+
+  public async setScales(scales: Scales): Promise<void> {
+    const res = await axios.post(`https://avatar.roblox.com/v1/avatar/set-scales`, {
+      scalesModel: scales
+    });
+
+    if(!res.data.success) return Promise.reject(res);
+
+    return;
+  }
+
+  public async setColors(colors: Colors) {
+    const res = await axios.post(`https://avatar.roblox.com/v1/avatar/set-scales`, {
+      bodyColorsModel: colors
+    });
+
+    if(!res.data.success) return Promise.reject(res);
+
+    return;
+  }
 }
 
 export default Roblox;
